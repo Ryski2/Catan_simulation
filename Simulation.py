4ef56ebfb424b9a0f995e36b228cabc2394d4754
pjiang99 <61607280+pjiang99@users.noreply.github.com>
2020-03-05 15:27:36 -0800

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,6 +1,7 @@
 from Board import Board
 from Connectors import BuildType
 from Player import Resource
+from Player import Strategies
 import random
 import itertools
 
@@ -11,33 +12,176 @@ class Simulation:
         self.board = Board(player_strategies, board_layout)
 
     def run(self):
-        print()
-        print("Players placing initial settlements and roads")
-        print("---------------------------------------------")
-        print()
-
-        self.first_turn()
-
-        print()
-        print("Moving on to main game section")
-        print("---------------------------------------------")
-        print()
-
-
-        winner = False
-        turns = 0
-        for i in range(0, max_turns):
-            print("Turn " + str(i + 1))
+        winner = None
+        while self.board.turns <= max_turns and winner is None:
             winner = self.game_turn()
-            if winner is not None:
-                turns = i
-                break
+        self.print_results(winner)
 
-        print()
-        print("---------------------------------------------")
+    def game_turn(self):
+        turns = self.board.turns
+        if turns == 0:
+            print()
+            print("Players placing initial settlements and roads")
+            print("---------------------------------------------")
+            print()
+            for player in self.board.players:
+                self.build(player);
+            # on second placement, players get the resources bordering their settlement
+            for player in reversed(self.board.players):
+                self.build(player);
+            for roll in range(1, 12):
+                if roll != 7:
+                    self.board.distribute(roll)
+            print()
+            print("Moving on to main game section")
+            print("---------------------------------------------")
+            print()
+        else:
+            print("Round " + str(self.board.turns))
+            for player in self.board.players:
+                print("\tPlayer " + str(player.id) + "'s Turn")
+                roll = self.board.dice_roll(player)
+                if roll == 7:
+                    for player1 in self.board.players:
+                        resources = player1.total_resources()
+                        if player1.total_resources() > 7:
+                            self.discard_half(player1)
+                            print("\t\t\tPlayer " + str(player1.id) + " had " + str(resources) + " resources and now has " + str(player1.total_resources()))
+                        else:
+                            print("\t\t\tPlayer " + str(player1.id) + " has " + str(resources) + " resouces. No cards are discarded")
+                    self.move_robber(player)
+                    self.steal(player)
+                else:
+                    self.board.distribute(roll)
+                self.trade(player)
+                self.build(player)
+                if player.win():
+                    return player
+        self.board.turns += 1
+
+    def trade(self, player):
+        strategies = player.strategies
+        if Strategies.Trade in strategies:
+            # trade with other players
+            for other in self.board.players:
+                if other is not player:
+                    player.trade_with_player(other)
+            # trade 4:1
+            player.trade_four_one()
+
+        # Default strategy: no trading
+
+
+
+    def build(self, player):
+        owned_nodes = player.nodes;
+        owned_edges = player.edges;
+        if self.board.turns == 0:
+            buildable_nodes = self.board.buildable_nodes;
+            buildable_edges = self.board.buildable_edges;
+        else:
+            buildable_nodes = player.buildable_nodes;
+            buildable_edges = player.buildable_edges;
+
+        strategies = player.strategies
+        if Strategies.Dummy in strategies:
+            """ADD STRATEGIES HERE"""
+            raise NotImplementedError()
+
+        else: # Default strategy
+            self.build_default(player, owned_nodes, owned_edges, buildable_nodes, buildable_edges)
+
+    # defauld building strategy
+    def build_default(self, player, owned_nodes, owned_edges, buildable_nodes, buildable_edges):
+        if self.board.turns == 0:
+            node_to_build = None
+            edge_to_build = None
+            while edge_to_build is None:
+                node_to_build = random.sample(buildable_nodes, 1)[0]
+                for edge in random.sample(node_to_build.edges, len(node_to_build.edges)):
+                    if edge is not None and edge.player is None:
+                        edge_to_build = edge
+                        break
+            self.board.build_road(player, edge_to_build)
+            self.board.build_settlement(player, node_to_build)
+        else:
+            if player.can_build_city():
+                #pick location to build
+                if len(player.settlements) != 0:
+                    city_node = random.sample(player.settlements, 1)[0]
+                    self.board.build_city(player, city_node)
+                else:
+                    print("\t\tPlayer " + str(player.id) + " had resources to bulid a city but no valid location was found")
+                #print("\t\tPlayer " + str(player.id) + " built a city at " + city_node.get_coords())
+            if player.can_build_settlement():
+                if len(buildable_nodes) != 0:
+                    settlement_node = random.sample(buildable_nodes, 1)[0]
+                    self.board.build_settlement(player, settlement_node)
+                else:
+                    print("\t\tPlayer " + str(player.id) + " had resources to bulid a settlement but no valid location was found")
+            if player.can_build_road() and len(player.buildable_nodes) == 0: #####
+                if len(buildable_edges) != 0:
+                    rode_edge = random.sample(buildable_edges, 1)[0]
+                    self.board.build_road(player, rode_edge)
+                else:
+                    print("\t\tPlayer " + str(player.id) + " had resources to bulid a road but no valid location was found")
+
+    def discard_half(self, player):
+        strategies = player.strategies
+        if Strategies.Dummy in strategies:
+            """ADD STRATEGIES HERE"""
+            raise NotImplementedError()
+
+        else: # Default strategy
+            num_discards = player.total_resources() // 2
+            discarded = 0
+            while (discarded < num_discards):
+                res = Resource(random.randint(1,5))
+                if player.resources[res] > 0:
+                    player.resources[res] -= 1
+                    discarded += 1
+
+    def move_robber(self, player):
+        strategies = player.strategies
+        if Strategies.Dummy in strategies:
+            """ADD STRATEGIES HERE"""
+            raise NotImplementedError()
+
+        else: # Default strategy
+            new_robber_tile = self.board.robber_tile
+            index = -1
+            while new_robber_tile == self.board.robber_tile:
+                new_robber_tile = random.choice(self.board.layout)
+            self.board.move_robber(new_robber_tile)
+
+    # "The person moving the robber must steal a card from a player adjacent to the robber if possible."
+    def steal(self, player):
+        stole_resource = False
+        other_player = None
+        res = None
+        strategies = player.strategies
+        if Strategies.Dummy in strategies:
+            """ADD STRATEGIES HERE"""
+            raise NotImplementedError()
+
+        else: # Default strategy
+            for node in random.sample(self.board.robber_tile.nodes, 6):
+                other_player = node.player
+                if other_player is not None and other_player is not player:
+                    res = Resource(random.randint(1, 5))
+                    if other_player.resources[res] > 0:
+                        stole_resource = True
+                        break
+        if stole_resource:
+            other_player.resources[res] -= 1
+            player.resources[res] += 1
+            print("\t\t\tPlayer " + str(player.id) + " stole one " + str(res) + " from Player " + str(other_player.id))
+        else:
+            print("\t\t\tNo resource to steal")
+
+    def print_results(self, winner):
         print("Game Ended")
-
-
+        print("---------------------------------------------")
         for player in self.board.players:
             #print every players settlements, roads, and cities
             print("Player " + str(player.id) + ": ")
@@ -49,179 +193,21 @@ class Simulation:
             for node in player.nodes:
                 if node.type == BuildType.City:
                     print("\t\t" + node.get_coords())
-            """
-            print("\tAvailable nodes:")
-            for node in player.buildable_nodes:
-                print("\t\t" + node.get_coords())
-            """
-
             print("\tEdge Between:")
             for edge in player.edges:
                 print("\t\t" + edge.node1.get_coords() + " to " + edge.node2.get_coords())
-            """
-            print("\tAvailable edges:")
-            for edge in player.buildable_edges:
-                print("\t\t" + edge.node1.get_coords() + " to " + edge.node2.get_coords())
-            """
 
         if winner is not None:
-            print("Player " + str(winner.id) + " Won After " + str(turns) + " Turns")
+            print("Player " + str(winner.id) + " Won After " + str(self.board.turns) + " Turns")
         else:
             print("After " + str(max_turns) + " turns, no one has reached 10 points")
             highest_score = -1
             for player in self.board.players:
                 if player.points() >= highest_score:
                     highest_score = player.points()
-
             winners = []
             for player in self.board.players:
                 if player.points() == highest_score:
                     winners.append(player.id)
             print("Player(s) " + ", ".join(str(p) for p in winners) + " had the most points, with " + str(highest_score) + " points")
-
         print()
-
-        for player in self.board.players:
-            print("Player " + str(player.id))
-            for resource in player.resources:
-                print("\t" + str(resource)+ ": " + str(player.resources[resource]))
-
-    def get_random_empty_node(self):
-        #taking random sample now stupid cause we don't loop through? maybe? idk
-        for node in random.sample(self.board.nodes, len(self.board.nodes)):
-            if (node.player == None):
-                return node
-
-    def get_random_empty_edge(self, node):
-        for edge in random.sample(node.edges, len(node.edges)):
-            if edge is not None and edge.player is None:
-                return edge
-
-    def initial_settlement_road_placement(self, player, collect_resources):
-        #first two settlements and roads are free, don't subtract from resources
-        #create a new list that contains all the nodes randomized (we don't actually randomize the original list here)
-        #loop through the list until you find one that's empty
-
-        found_node_edge = False
-        while not found_node_edge:
-            node = self.get_random_empty_node()
-            if node is None:
-                raise ValueException("No empty nodes found -- this shouldn't happen in the first round so if you're seeing this the code's wrong")
-            edge = self.get_random_empty_edge(node)
-
-            #note: a node could be empty but all surrounding edges could be taken; if this happesn, we need to pick a new node
-            if edge is not None:
-                found_node_edge = True
-                self.build_road(player, edge)
-                self.build_settlement(player, node)
-                """
-                print("Player " + str(player.id) + " built a settlement at coordinates" +\
-                              node.get_coords() + ", and a road to " +\
-                              #make sure we're giving the coordinates to the other node, not the node where the settlement is
-                              (edge.node1.get_coords() if edge.node2 is node else edge.node2.get_coords()))
-                """
-                #on second round of placement, give each player one resource of each of the bordering tiles
-                if (collect_resources):
-                    print("\tPlayer " + str(player.id) + " collected the following: " + ", ".join(str(tile.resource) for tile in node.resources))
-                    for tile in node.resources:
-                        resource = tile.resource
-                        if resource == Resource.Desert:
-                            continue
-                        node.player.resources[resource] += 1
-
-    # Build a road for player on edge
-    def build_road(self, player, edge):
-        if player.strategy is None:
-            player.build_road()
-            edge.player = player
-            self.board.buildable_edges.discard(edge)
-            player.edges.add(edge)
-            for player1 in self.board.players:
-                player1.buildable_edges.discard(edge)
-            for edge1 in itertools.chain(edge.node1.edges, edge.node2.edges):
-                if edge1 is not None and edge1 != edge and edge1 in self.board.buildable_edges:
-                    player.buildable_edges.add(edge1)
-            player.buildable_nodes.add(edge.node1)
-            player.buildable_nodes.add(edge.node2)
-        """
-        elif player.strategy is ___:
-            ...
-        """
-        print("\t\tPlayer " + str(player.id) + " built a road from " + edge.node1.get_coords() + " to " + edge.node2.get_coords())
-
-    # Build a settlement for player at node
-    def build_settlement(self, player, node):
-        if player.strategy is None:
-            player.build_settlement()
-            node.type = BuildType.Settlement
-            node.player = player
-            player.nodes.add(node)
-            nodes = {node}
-            for edge in node.edges:
-                if edge is not None:
-                    nodes.add(edge.get_other_node(node))
-            for node1 in nodes:
-                self.board.buildable_nodes.discard(node1)
-                for player1 in self.board.players:
-                    player1.buildable_nodes.discard(node1)
-        """
-        elif player.strategy is ___:
-            ...
-        """
-        print("\t\tPlayer " + str(player.id) + " built a settlement at " + node.get_coords())
-
-    def find_node_with_settlement(self, player):
-        if len(player.nodes) != 0:
-            return random.sample(player.nodes, 1)[0]
-
-    def first_turn(self):
-        for player in self.board.players:
-            self.initial_settlement_road_placement(player, False)
-
-        #on second placement, players get the resources bordering their settlement
-        for player in reversed(self.board.players):
-            self.initial_settlement_road_placement(player, True)
-
-    def find_valid_settlement_location(self, player):
-        if len(player.buildable_nodes) != 0:
-            return random.sample(player.buildable_nodes, 1)[0]
-
-    def find_valid_road_location(self, player):
-        if len(player.buildable_edges) != 0:
-            return random.sample(player.buildable_edges, 1)[0]
-
-    #each player rolls dice and acquires resources/places settlements
-    #this is the bulk of the game, and includes building and rolling the dice
-    def game_turn(self):
-        for player in self.board.players:
-            print("\tPlayer " + str(player.id) + "'s Turn")
-            self.board.dice_roll()
-
-            #if possible, build
-            if player.can_build_city():
-                #pick location to build
-                city_node = self.find_node_with_settlement(player)
-                if city_node is None:
-                    print("\t\tPlayer " + str(player.id) + " had resources to bulid a city but no valid location was found")
-                else:
-                    player.build_city()
-                    city_node.type = BuildType.City
-                    print("\t\tPlayer " + str(player.id) + " built a city at " + city_node.get_coords())
-
-            if player.can_build_settlement():
-                settlement_node = self.find_valid_settlement_location(player)
-                if settlement_node is None:
-                    print("\t\tPlayer " + str(player.id) + " had resources to bulid a settlement but no valid location was found")
-                else:
-                    self.build_settlement(player, settlement_node)
-
-            if player.can_build_road() and len(player.buildable_nodes) == 0:
-                road_edge = self.find_valid_road_location(player)
-                if road_edge is None:
-                    print("\t\tPlayer " + str(player.id) + " had resources to bulid a road but no valid location was found")
-                else:
-                    self.build_road(player, road_edge)
-
-            #check if anyone is winning
-            if player.points() >= 10:
-                return player
