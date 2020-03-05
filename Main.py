731f2a3b0f6b167204fc9f95106120746165549c
pjiang99 <61607280+pjiang99@users.noreply.github.com>
2020-03-05 15:24:56 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,22 @@
 from Simulation import *
+from Player import Strategies
 import random
 import sys
 
 seed = random.randrange(sys.maxsize)
 random.seed(seed)
+print("Seed: " + str(seed))
 
 # run simulation once with four players who have no strategyes with the basic board layout (basic_layout.jpg).
-sim = Simulation( [None] * 4, "basic")
+
+# each player can carry a set of strategies
+# Multiple stratgies can coexist.
+# For example, a building strategy and a trading strategy don't comflict
+strats = [{Strategies.Trade}] + [set()] * 3
+sim = Simulation(strats, "basic")
 sim.run()
 print("Seed: " + str(seed))
+"""
+blockPrint()
+enablePrint()
+"""
