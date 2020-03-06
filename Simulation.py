871d426a97d146157434daff12cc79cd71f422b1
Ryan Lisowski <rlisowski2@gmail.com>
2020-03-05 22:21:00 -0800

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -64,9 +64,7 @@ class Simulation:
                     while player.can_build_city() and player.settlementCount > 0 \
                     or player.can_build_settlement() and len(player.buildable_nodes) != 0 \
                     or player.can_build_road() and len(player.buildable_edges) != 0:
-                        print(player.can_build_city(),player.can_build_settlement(),player.can_build_road(), len(player.buildable_edges),player.resources)
                         self.build(player)
-                        print(player.can_build_city(),player.can_build_settlement(),player.can_build_road(), len(player.buildable_edges),player.resources)
                 else:
                     self.build(player)
                 if player.win():
